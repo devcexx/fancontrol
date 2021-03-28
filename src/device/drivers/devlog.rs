235d27a189c9b75d584f5d67c7eaa5bc8a88ae5c
Roberto Guillén <robertoguillen@me.com>
2021-03-28 23:43:13 +0200

    Fixed extra [ on drivers logging

--- src/device/drivers/devlog.rs
+++ src/device/drivers/devlog.rs
@@ -1,6 +1,6 @@
 #[macro_export]
 macro_rules! driver_log_define {
     ($driver:literal, $prefix:ident) => {
-        targeted_log::targeted_log!(std::concat!("[drivers::", $driver, " {}"), $prefix);
+        targeted_log::targeted_log!(std::concat!("drivers::", $driver, " {}"), $prefix);
     };
 }
